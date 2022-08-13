f5971ffb14b281328470112f978c57b3b8a22576
Riku-programming <riku_programming@gmail.com>
2022-08-13 22:37:00 +0900

    csvを作成せずscrapingの処理をするように変更 benchmarkを測るテストを作成

--- main.go
+++ main.go
@@ -1,58 +1,55 @@
 package main
 
 import (
-	"encoding/csv"
-	"log"
-	"os"
-	"time"
-
+	"fmt"
 	"github.com/gocolly/colly"
+	"log"
+	"time"
 )
 
-func main() {
-	scraping_csv("data.csv", "https://award.tabelog.com/hyakumeiten/izakaya/2021/")
+type ShopDetail struct {
+	PhoneNumber   string
+	ReserveStatus string
+	Address       string
+	Time          string
+	Payment       string
 }
 
-func scraping_csv(fileName string, URL string) {
-	fName := fileName
-	// csvを作成する
-	file, err := os.Create(fName)
-	if err != nil {
-		log.Fatalf("Could not create file, err :%q", err)
-		return
-	}
-	defer file.Close()
+func main() {
+	scraping("https://award.tabelog.com/hyakumeiten/izakaya/2021/")
+}
 
-	// csv操作インスタンス
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	writer.Write([]string{"ShopName", "Area", "Holiday", "Rating", "Target"})
-
+func scraping(URL string) {
+	shopDetails := make([]ShopDetail, 0)
 	// collyインスタンス
 	c := colly.NewCollector()
-
-	// html -> .internship_meta要素にアクセス
+	detailCollector := c.Clone()
+	// html -> .hyakumeiten-shop__item要素にアクセス
 	c.OnHTML(".hyakumeiten-shop__item", func(e *colly.HTMLElement) {
-		writer.Write([]string{
-			//店名
-			e.ChildText(".hyakumeiten-shop__name"),
-			//定休日
-			e.ChildText(".hyakumeiten-shop__holiday"),
-			//住所
-			e.ChildText(".hyakumeiten-shop__area"),
-			// 評価
-			e.ChildText(".hyakumeiten-shop__rating"),
-			//詳細リンク
-			e.ChildAttr(".hyakumeiten-shop__target", "href"),
-		})
+		//詳細リンク
+		link := e.ChildAttr(".hyakumeiten-shop__target", "href")
+		detailCollector.Visit(e.Request.AbsoluteURL(link))
 	})
-
-	start := time.Now()
+	detailCollector.OnHTML(".rstinfo-table__table", func(e *colly.HTMLElement) {
+		shopDetail := ShopDetail{
+			PhoneNumber:   e.ChildText(".rstinfo-table__tel-num"),
+			ReserveStatus: e.ChildText(".rstinfo-table__reserve-status"),
+			Address:       e.ChildText(".rstinfo-table__reserve-address"),
+			Time:          e.ChildText(".rstinfo-table__subject-text"),
+			Payment:       e.ChildText(".rstinfo-table__pay-item"),
+		}
+		shopDetails = append(shopDetails, shopDetail)
+	})
 	// スクレイピング
+	c.OnRequest(func(request *colly.Request) {
+		fmt.Println("ここにリクエストするよ", request.URL.String())
+
+	})
+	start := time.Now()
 	c.Visit(URL)
 
 	// 後処理
+	fmt.Println(shopDetails)
 	end := time.Now()
 	log.Println("Scraping Complete")
 	log.Printf("Time:%v", end.Sub(start))
